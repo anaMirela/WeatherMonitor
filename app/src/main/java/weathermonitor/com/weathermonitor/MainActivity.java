e9b7be228f4a2390804e89246da4b0cc70ba8fa4
ana mirela <ana.mirela.irimia@gmail.com>
2016-12-06 14:45:01 +0200

    added map + two markers

--- app/src/main/java/weathermonitor/com/weathermonitor/MainActivity.java
+++ app/src/main/java/weathermonitor/com/weathermonitor/MainActivity.java
@@ -1,13 +1,55 @@
 package weathermonitor.com.weathermonitor;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng bucharest = new LatLng(44.426, 26.102);
+        LatLng timisoara = new LatLng(45.748, 21.208);
+
+        // list of markers
+        List<Marker> markers = new ArrayList<Marker>();
+        Marker newMarker = map.addMarker(new MarkerOptions().position(bucharest).title("Marker for Bucharest"));
+        markers.add(newMarker);
+        newMarker = map.addMarker(new MarkerOptions().position(timisoara).title("Marker for Bucharest"));
+        markers.add(newMarker);
+
+        // calculate bounds for the markers on the map
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (Marker m : markers) {
+            builder.include(m.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds,250,250, 0));
     }
 }
