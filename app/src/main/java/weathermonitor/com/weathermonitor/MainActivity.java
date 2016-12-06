7d43b95e42875da1d5c70c4102a9706c93babe42
ana mirela <ana.mirela.irimia@gmail.com>
2016-12-06 17:38:06 +0200

    added request to server to get recent data

--- app/src/main/java/weathermonitor/com/weathermonitor/MainActivity.java
+++ app/src/main/java/weathermonitor/com/weathermonitor/MainActivity.java
@@ -1,8 +1,11 @@
 package weathermonitor.com.weathermonitor;
 
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,21 +16,43 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
 
+    private RequestFactory requestFactory;
     private GoogleMap map;
+    private int lastDataId = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestFactory = new RequestFactory();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+
+            public void run() {
+                Log.i("test", "thread");
+                new GetRecentData().execute();
+                handler.postDelayed(this, 20000);
+            }
+        };
+        runnable.run();
     }
 
     @Override
@@ -51,5 +76,42 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
         LatLngBounds bounds = builder.build();
         map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds,250,250, 0));
+    }
+
+    private class GetRecentData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String query = "?action=recentdata&lastId=" + lastDataId;
+
+            InputStream is = requestFactory.createGetRequest(query);
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(is));
+            try {
+                String dataJsonArray = inputStream.readLine();
+                if (dataJsonArray != null) {
+                    System.out.println(">>>>>>>>" + dataJsonArray);
+                } else {
+                    System.out.println(">>>>>>NULL");
+                }
+
+                JSONArray jsonArray = new JSONArray(dataJsonArray);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    System.out.println(">>>>>>>>" + jsonArray.get(i).toString());
+                    JSONObject jsonObject = new JSONObject(jsonArray.get(i).toString());
+                    // TODO - prelucreaza datele
+                    lastDataId = Integer.valueOf(jsonObject.getString("id"));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
     }
 }
---
+++ app/src/main/java/weathermonitor/com/weathermonitor/RequestFactory.java
@@ -0,0 +1,32 @@
+package weathermonitor.com.weathermonitor;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Mi on 12/6/2016.
+ */
+
+public class RequestFactory {
+
+    public static final String SERVER_URL ="http://192.168.0.100/server_weather_app/index.php";//"http://192.168.0.100/server_weather_app/index.php";
+
+    public InputStream createGetRequest(String query) {
+        HttpURLConnection con;
+        try {
+            con = (HttpURLConnection) new URL(SERVER_URL + query).openConnection();
+            Log.i("tag", con.getResponseCode() + "");
+            return con.getInputStream();
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
